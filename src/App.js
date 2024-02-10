485503906cc2e5df921a8ddd672f78f1d5c34bcb
Puru <puruusharmaaa@gmail.com>
2024-02-10 13:57:57 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ const contactsData = [
     title: "GitHub",
   },
   {
-    link: "https://drive.google.com/file/d/1CNfdY1QEuP9lKAkmn9BZoBO8pDFGW54y/view?usp=sharing",
+    link: "https://drive.google.com/file/d/1Y6C7piLJCZ6iZP9HnGoqRHvgmWkyDzvb/view?usp=sharing",
     image: "./assets/Resume.jpg",
     title: "Resume",
   },
